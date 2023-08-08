a8ad27083614cb14b8c9be69005cc81f97896ce4
DanielPeterA <danielpeter9627@gmail.com>
2023-08-08 22:58:28 +0530

    Message

--- mongodb.js
+++ mongodb.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
-const DB_URL = process.env.DB_URL
-mongoose.connect(DB_URL,{useNewUrlParser:true,useUnifiedTopology:true})
-.then(()=>console.log('Mongoose Connection Succesfull')).catch((err)=>console.log(err));
+
+mongoose.connect("mongodb://0.0.0.0:27017",{useNewUrlParser:true,useUnifiedTopology:true})
+.then(()=>console.log('Mongoose Connection Succesfull')).catch((err)=>console.log(`Mongoose connection error DP`,err));
 
 
 const Schemamodel = mongoose.Schema({

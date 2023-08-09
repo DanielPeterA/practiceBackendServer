8576e367ee9a5c909b97288470e46fac4485cc2c
DanielPeterA <danielpeter9627@gmail.com>
2023-08-09 12:35:55 +0530

    V 1.0

--- mongodb.js
+++ mongodb.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
+require('dotenv').config({path:'./.env'})
+const DB_URL = process.env.DB_URL
 
-mongoose.connect("mongodb://0.0.0.0:27017",{useNewUrlParser:true,useUnifiedTopology:true})
+mongoose.connect(`${DB_URL}`,{useNewUrlParser:true,useUnifiedTopology:true})
 .then(()=>console.log('Mongoose Connection Succesfull')).catch((err)=>console.log(`Mongoose connection error DP`,err));
 
 
